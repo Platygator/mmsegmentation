f51fb5d3daadb086bbc30f6b3cf4b108081579b0
jan <j.schiff96@gmail.com>
2021-01-30 22:51:27 +0100

    removed all data moved to the custom deeplab descriptor

--- own_tests/deeplabV3p_test.py
+++ own_tests/deeplabV3p_test.py
@@ -16,26 +16,11 @@ from mmcv import Config
 import mmcv
 from mmseg.apis import set_random_seed
 
-cfg = Config.fromfile('configs/_base_/models/deeplabv3plus_r50-d8.py')
+cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py')
 # cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_cityscapes.py')
 # cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes.py')
-data_root = 'data/boulderColabFormat'
-img_dir = 'images'
-ann_dir = 'labels'
-split_dir = 'splits'
 
-# split train/val set randomly
-mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
-filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
-    osp.join(data_root, ann_dir), suffix='.png')]
-with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
-    # select first 4/5 as train set
-    train_length = int(len(filename_list)*4/5)
-    f.writelines(line + '\n' for line in filename_list[:train_length])
-    print(filename_list[3])
-with open(osp.join(data_root, split_dir, 'val.txt'), 'w') as f:
-    # select last 1/5 as train set
-    f.writelines(line + '\n' for line in filename_list[train_length:])
+
 
 # Since we use ony one GPU, BN is used instead of SyncBN
 cfg.norm_cfg = dict(type='BN', requires_grad=True)
@@ -47,69 +32,7 @@ cfg.model.decode_head.num_classes = 8
 cfg.model.auxiliary_head.num_classes = 8
 # cfg.model.backbone.with_cp = True
 
-# Modify dataset type and path
-cfg.dataset_type = 'BoulderDataset'
-cfg.data_root = data_root
 
-cfg.data.samples_per_gpu = 2
-cfg.data.workers_per_gpu = 2
-
-cfg.img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-cfg.crop_size = (256, 256)
-cfg.train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(752, 480), ratio_range=(0.5, 2.0)),
-    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5),
-    dict(type='PhotoMetricDistortion'),
-    dict(type='Normalize', **cfg.img_norm_cfg),
-    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
-]
-
-cfg.test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(752, 480),
-        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **cfg.img_norm_cfg),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
-
-
-cfg.data.train.type = cfg.dataset_type
-cfg.data.train.data_root = cfg.data_root
-cfg.data.train.img_dir = img_dir
-cfg.data.train.ann_dir = ann_dir
-cfg.data.train.pipeline = cfg.train_pipeline
-cfg.data.train.split = 'splits/train.txt'
-
-cfg.data.val.type = cfg.dataset_type
-cfg.data.val.data_root = cfg.data_root
-cfg.data.val.img_dir = img_dir
-cfg.data.val.ann_dir = ann_dir
-cfg.data.val.pipeline = cfg.test_pipeline
-cfg.data.val.split = 'splits/val.txt'
-
-cfg.data.test.type = cfg.dataset_type
-cfg.data.test.data_root = cfg.data_root
-cfg.data.test.img_dir = img_dir
-cfg.data.test.ann_dir = ann_dir
-cfg.data.test.pipeline = cfg.test_pipeline
-cfg.data.test.split = 'splits/val.txt'
-
-# We can still use the pre-trained Mask RCNN model though we do not need to
-# use the mask branch
 # cfg.load_from = 'checkpoints/deeplabv3plus_r50-d8_512x1024_40k_cityscapes_20200605_094610-d222ffcd.pth'
 # cfg.load_from = 'checkpoints/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes_20201226_090828-e451abd9.pth'
 # cfg.resume_from = 'work_dirs/deeplab_test/iter_200.pth'
