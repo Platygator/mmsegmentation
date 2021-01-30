48372c4eae0c3b3ee2fe27fde505674ef60c2ae7
jan <j.schiff96@gmail.com>
2021-01-30 22:49:53 +0100

    smaller crop

--- configs/_base_/datasets/boulderset.py
+++ configs/_base_/datasets/boulderset.py
@@ -1,9 +1,11 @@
-# dataset settings
+# dataset settings adapted from cityscapes.py
+# By Jan Schiffeler
 dataset_type = 'BoulderDataset'
 data_root = 'data/boulderColabFormat/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 1024)
+# crop_size = (512, 1024)
+crop_size = (256, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
