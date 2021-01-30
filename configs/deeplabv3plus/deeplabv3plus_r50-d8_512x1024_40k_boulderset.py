d4c5830391104636af499e9372c3adab2ba26ed5
jan <j.schiff96@gmail.com>
2021-01-30 22:50:54 +0100

    now using r18

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
@@ -1,5 +1,6 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8.py',
-    '../_base_/datasets/boulder_set.py', '../_base_/default_runtime.py',
+    '../_base_/models/deeplabv3plus_r18-d8.py',
+    # '../_base_/models/deeplabv3plus_r50-d8.py',
+    '../_base_/datasets/boulderset.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_40k.py'
 ]
