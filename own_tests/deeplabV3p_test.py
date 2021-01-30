65f8e1b0e517e6b5724ec244be356c3cf50f8574
jan <j.schiff96@gmail.com>
2021-01-30 12:29:51 +0100

    tutorial hacked together, runs inference does not work

---
+++ own_tests/deeplabV3p_test.py
@@ -0,0 +1,162 @@
+from mmseg.datasets.builder import DATASETS
+from mmseg.datasets.custom import CustomDataset
+import os.path as osp
+
+@DATASETS.register_module()
+class BoulderDataset(CustomDataset):
+  CLASSES = ("background", "stone", "border")
+  PALETTE = [0, 1, 2]
+  def __init__(self, split, **kwargs):
+    super().__init__(img_suffix='.png', seg_map_suffix='.png',
+                     split=split, **kwargs)
+    assert osp.exists(self.img_dir) and self.split is not None
+
+
+from mmcv import Config
+import mmcv
+from mmseg.apis import set_random_seed
+
+cfg = Config.fromfile('configs/_base_/models/deeplabv3plus_r50-d8.py')
+# cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_cityscapes.py')
+# cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes.py')
+data_root = 'data/boulderColabFormat'
+img_dir = 'images'
+ann_dir = 'labels'
+split_dir = 'splits'
+
+# split train/val set randomly
+mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
+filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
+    osp.join(data_root, ann_dir), suffix='.png')]
+with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
+    # select first 4/5 as train set
+    train_length = int(len(filename_list)*4/5)
+    f.writelines(line + '\n' for line in filename_list[:train_length])
+    print(filename_list[3])
+with open(osp.join(data_root, split_dir, 'val.txt'), 'w') as f:
+    # select last 1/5 as train set
+    f.writelines(line + '\n' for line in filename_list[train_length:])
+
+# Since we use ony one GPU, BN is used instead of SyncBN
+cfg.norm_cfg = dict(type='BN', requires_grad=True)
+cfg.model.backbone.norm_cfg = cfg.norm_cfg
+cfg.model.decode_head.norm_cfg = cfg.norm_cfg
+cfg.model.auxiliary_head.norm_cfg = cfg.norm_cfg
+# modify num classes of the model in decode/auxiliary head
+cfg.model.decode_head.num_classes = 8
+cfg.model.auxiliary_head.num_classes = 8
+# cfg.model.backbone.with_cp = True
+
+# Modify dataset type and path
+cfg.dataset_type = 'BoulderDataset'
+cfg.data_root = data_root
+
+cfg.data.samples_per_gpu = 2
+cfg.data.workers_per_gpu = 2
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (256, 256)
+cfg.train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(752, 480), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **cfg.img_norm_cfg),
+    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(752, 480),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+
+cfg.data.train.type = cfg.dataset_type
+cfg.data.train.data_root = cfg.data_root
+cfg.data.train.img_dir = img_dir
+cfg.data.train.ann_dir = ann_dir
+cfg.data.train.pipeline = cfg.train_pipeline
+cfg.data.train.split = 'splits/train.txt'
+
+cfg.data.val.type = cfg.dataset_type
+cfg.data.val.data_root = cfg.data_root
+cfg.data.val.img_dir = img_dir
+cfg.data.val.ann_dir = ann_dir
+cfg.data.val.pipeline = cfg.test_pipeline
+cfg.data.val.split = 'splits/val.txt'
+
+cfg.data.test.type = cfg.dataset_type
+cfg.data.test.data_root = cfg.data_root
+cfg.data.test.img_dir = img_dir
+cfg.data.test.ann_dir = ann_dir
+cfg.data.test.pipeline = cfg.test_pipeline
+cfg.data.test.split = 'splits/val.txt'
+
+# We can still use the pre-trained Mask RCNN model though we do not need to
+# use the mask branch
+# cfg.load_from = 'checkpoints/deeplabv3plus_r50-d8_512x1024_40k_cityscapes_20200605_094610-d222ffcd.pth'
+# cfg.load_from = 'checkpoints/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes_20201226_090828-e451abd9.pth'
+# cfg.resume_from = 'work_dirs/deeplab_test/iter_200.pth'
+
+# Set up working dir to save files and logs.
+cfg.work_dir = './work_dirs/deeplab_test'
+
+cfg.total_iters = 200
+cfg.log_config.interval = 10
+cfg.evaluation.interval = 200
+cfg.checkpoint_config.interval = 200
+
+# Set seed to facitate reproducing the result
+cfg.seed = 0
+set_random_seed(0, deterministic=False)
+cfg.gpu_ids = range(1)
+
+# Let's have a look at the final config used for training
+print(f'Config:\n{cfg.pretty_text}')
+
+
+from mmseg.datasets import build_dataset
+from mmseg.models import build_segmentor
+from mmseg.apis import train_segmentor
+
+
+# Build the dataset
+datasets = [build_dataset(cfg.data.train)]
+
+# Build the detector
+model = build_segmentor(cfg.model)
+# Add an attribute for visualization convenience
+model.CLASSES = datasets[0].CLASSES
+
+# Create work_dir
+mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+train_segmentor(model, datasets, cfg, distributed=False, validate=True,
+                meta=dict())
+
+
+from mmseg.apis import inference_segmentor, show_result_pyplot
+import matplotlib.pyplot as plt
+
+img = mmcv.imread('data/test_img/03_02_01450.png')
+palette = [0, 1, 2]
+
+model.cfg = cfg
+result = inference_segmentor(model, img)
+plt.figure(figsize=(8, 6))
+show_result_pyplot(model, img, result, palette)
