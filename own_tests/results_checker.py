754082d3951062fe0a67f394ddcfb097b53b6f19
jan <j.schiff96@gmail.com>
2021-02-02 09:58:05 +0100

    trying to get the mask image

--- own_tests/results_checker.py
+++ own_tests/results_checker.py
@@ -1,11 +1,14 @@
 import mmcv
 import numpy as np
 
-img = mmcv.imread('data/boulderColabFormat/images/03_02_00750.png')
+img = mmcv.imread('data/BoulderDataset/images/03_02_00750.png')
 res = mmcv.imread('data/results/03_02_00750.png')
 
 diff = img-res
 
+img_half = np.floor(img/2)
+diff2 = img_half-res
+diff2_amp = np.ceil(diff2*127.5)
 print(diff.any())
 
 mmcv.imshow(diff*127, "results", 2000)
