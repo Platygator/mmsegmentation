66236a7918aec85232f83d8e104c177beb862a55
jan <j.schiff96@gmail.com>
2021-02-02 09:56:45 +0100

    init commit

---
+++ own_tests/create_split.py
@@ -0,0 +1,20 @@
+import mmcv
+import os.path as osp
+
+data_root = 'data/BoulderDataset'
+img_dir = 'images'
+ann_dir = 'labels'
+split_dir = 'splits'
+
+# split train/val set randomly
+mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
+filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
+    osp.join(data_root, ann_dir), suffix='.png')]
+with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
+    # select first 4/5 as train set
+    train_length = int(len(filename_list)*4/5)
+    f.writelines(line + '\n' for line in filename_list[:train_length])
+    print(filename_list[3])
+with open(osp.join(data_root, split_dir, 'val.txt'), 'w') as f:
+    # select last 1/5 as train set
+    f.writelines(line + '\n' for line in filename_list[train_length:])
