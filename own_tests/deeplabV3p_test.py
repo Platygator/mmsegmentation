69a56388396733c9cc6ba88dcdcbb153de87f10d
jan <j.schiff96@gmail.com>
2021-02-02 09:57:46 +0100

    option to chose between training and testing. outsourced setting of cfg.norm_cfg and the number of classes

--- own_tests/deeplabV3p_test.py
+++ own_tests/deeplabV3p_test.py
@@ -13,23 +13,11 @@ import matplotlib.pyplot as plt
 
 
 cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py')
-# cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_cityscapes.py')
-# cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes.py')
-
-# Since we use ony one GPU, BN is used instead of SyncBN
-cfg.norm_cfg = dict(type='BN', requires_grad=True)
-cfg.model.backbone.norm_cfg = cfg.norm_cfg
-cfg.model.decode_head.norm_cfg = cfg.norm_cfg
-cfg.model.auxiliary_head.norm_cfg = cfg.norm_cfg
-# modify num classes of the model in decode/auxiliary head
-cfg.model.decode_head.num_classes = 8
-cfg.model.auxiliary_head.num_classes = 8
 # cfg.model.backbone.with_cp = True
 
-
-# cfg.load_from = 'checkpoints/deeplabv3plus_r50-d8_512x1024_40k_cityscapes_20200605_094610-d222ffcd.pth'
 # cfg.load_from = 'checkpoints/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes_20201226_090828-e451abd9.pth'
-cfg.resume_from = 'work_dirs/deeplab_test/latest.pth'
+# cfg.resume_from = 'work_dirs/deeplab_test/latest.pth'
+cfg.resume_from = 'work_dirs/deeplab_test/checkpoint_grey.pth'
 
 # Set up working dir to save files and logs.
 cfg.work_dir = './work_dirs/deeplab_test'
@@ -37,7 +25,7 @@ cfg.work_dir = './work_dirs/deeplab_test'
 cfg.total_iters = 200
 cfg.log_config.interval = 10
 cfg.evaluation.interval = 200
-cfg.checkpoint_config.interval = 200
+cfg.checkpoint_config.interval = 400
 
 # Set seed to facitate reproducing the result
 cfg.seed = 0
@@ -54,17 +42,19 @@ datasets = [build_dataset(cfg.data.train)]
 model = build_segmentor(cfg.model)
 # Add an attribute for visualization convenience
 model.CLASSES = datasets[0].CLASSES
+model.PALETTE = datasets[0].PALETTE
 
-# Create work_dir
-mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
-train_segmentor(model, datasets, cfg, distributed=False, validate=True,
-                meta=dict())
+train = True
+if train:
+    # Create work_dir
+    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+    train_segmentor(model, datasets, cfg, distributed=False, validate=True,
+                    meta=dict())
+else:
+    img = mmcv.imread('data/test_img/03_02_01450.png')
+    palette = [0, 1, 2]
 
-
-img = mmcv.imread('data/test_img/03_02_01450.png')
-palette = [0, 1, 2]
-
-model.cfg = cfg
-result = inference_segmentor(model, img)
-plt.figure(figsize=(8, 6))
-show_result_pyplot(model, img, result, palette)
+    model.cfg = cfg
+    result = inference_segmentor(model, img)
+    plt.figure(figsize=(8, 6))
+    show_result_pyplot(model, img, result, palette)
