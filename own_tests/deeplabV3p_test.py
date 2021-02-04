388f082184b1e9ace719928f585039781e19e55a
jan <j.schiff96@gmail.com>
2021-02-04 10:26:08 +0100

    removed train/test statement changed some comment and printouts

--- own_tests/deeplabV3p_test.py
+++ own_tests/deeplabV3p_test.py
@@ -17,7 +17,7 @@ cfg = Config.fromfile('configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_b
 
 # cfg.load_from = 'checkpoints/deeplabv3plus_r18b-d8_512x1024_80k_cityscapes_20201226_090828-e451abd9.pth'
 # cfg.resume_from = 'work_dirs/deeplab_test/latest.pth'
-cfg.resume_from = 'work_dirs/deeplab_test/checkpoint_grey.pth'
+# cfg.resume_from = 'work_dirs/deeplab_test/checkpoint_grey.pth'
 
 # Set up working dir to save files and logs.
 cfg.work_dir = './work_dirs/deeplab_test'
@@ -33,7 +33,7 @@ set_random_seed(0, deterministic=False)
 cfg.gpu_ids = range(1)
 
 # Let's have a look at the final config used for training
-print(f'Config:\n{cfg.pretty_text}')
+# print(f'Config:\n{cfg.pretty_text}')
 
 # Build the dataset
 datasets = [build_dataset(cfg.data.train)]
@@ -44,17 +44,16 @@ model = build_segmentor(cfg.model)
 model.CLASSES = datasets[0].CLASSES
 model.PALETTE = datasets[0].PALETTE
 
-train = True
-if train:
-    # Create work_dir
-    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
-    train_segmentor(model, datasets, cfg, distributed=False, validate=True,
-                    meta=dict())
-else:
-    img = mmcv.imread('data/test_img/03_02_01450.png')
-    palette = [0, 1, 2]
 
-    model.cfg = cfg
-    result = inference_segmentor(model, img)
-    plt.figure(figsize=(8, 6))
-    show_result_pyplot(model, img, result, palette)
+# Create work_dir
+mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+train_segmentor(model, datasets, cfg, distributed=False, validate=True,
+                meta=dict())
+
+img = mmcv.imread('data/test_img/03_02_01450.png')
+palette = [0, 1, 2]
+
+model.cfg = cfg
+result = inference_segmentor(model, img)
+plt.figure(figsize=(8, 6))
+show_result_pyplot(model, img, result, palette)
