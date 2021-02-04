b67e4916982e162bd72374aa411d40b3f8d3ba2d
jan <j.schiff96@gmail.com>
2021-02-04 10:23:19 +0100

    samples_per_gpu back to 2

--- configs/_base_/datasets/boulderset.py
+++ configs/_base_/datasets/boulderset.py
@@ -36,7 +36,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,  # was 2
+    samples_per_gpu=2,  # was 2
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
