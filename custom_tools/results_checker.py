652e99b88d343b66bce671dbbc1dd1d5a26c13ac
jan <j.schiff96@gmail.com>
2021-02-05 09:56:48 +0100

    test for live segmenationa

--- custom_tools/results_checker.py
+++ custom_tools/results_checker.py
@@ -2,9 +2,11 @@ import mmcv
 import numpy as np
 
 img = mmcv.imread('data/boulderColabFormat/labels/03_02_00750.png')
-res = mmcv.imread('data/results/03_02_00750.png', 0)
+res = mmcv.imread('data/results/03_02_00750.png')
+res_live = mmcv.imread('data/live_test.png')
 
 # res = np.floor(res * res * 63.75).astype('uint8')
 res = np.floor(res * 127.5).astype('uint8')
+res_live = np.floor(res_live * 127.5).astype('uint8')
 
 mmcv.imwrite(res, 'data/ground_truth_03_02_00750.png')
