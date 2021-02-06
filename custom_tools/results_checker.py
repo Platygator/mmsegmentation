cae68e3f2cf72f17de8418a87d1d65ca2c80f5e4
jan <j.schiff96@gmail.com>
2021-02-06 13:10:32 +0100

    debug calculations

--- custom_tools/results_checker.py
+++ custom_tools/results_checker.py
@@ -6,7 +6,9 @@ res = mmcv.imread('data/results/03_02_00750.png')
 res_live = mmcv.imread('data/live_test.png')
 
 # res = np.floor(res * res * 63.75).astype('uint8')
+img = np.floor(img * 127.5).astype('uint8')
 res = np.floor(res * 127.5).astype('uint8')
 res_live = np.floor(res_live * 127.5).astype('uint8')
 
 mmcv.imwrite(res, 'data/ground_truth_03_02_00750.png')
+pip
