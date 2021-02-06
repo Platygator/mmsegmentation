520e48313db91045f849cb616edf545af8f64dd0
jan <j.schiff96@gmail.com>
2021-02-06 13:10:20 +0100

    wrote changes to the base/models locally

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
@@ -1,6 +1,26 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r18-d8.py',
-    # '../_base_/models/deeplabv3plus_r50-d8.py',
+    # '../_base_/models/deeplabv3plus_r18-d8.py',
+    '../_base_/models/deeplabv3plus_r50-d8.py',
     '../_base_/datasets/boulderset.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_40k.py'
 ]
+
+norm_cfg = dict(type='BN', requires_grad=True)
+num_classes = 3
+model = dict(
+    pretrained='torchvision://resnet18',
+    backbone=dict(type='ResNet', depth=18),
+    decode_head=dict(
+        c1_in_channels=64,
+        c1_channels=12,
+        in_channels=512,
+        channels=128,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg
+    ),
+    auxiliary_head=dict(
+        in_channels=256,
+        channels=64,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg
+    ))
