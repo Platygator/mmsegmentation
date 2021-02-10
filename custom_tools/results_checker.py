f7066bd097df614fa20ed7303d5625fe582065f8
jan <j.schiff96@gmail.com>
2021-02-10 10:16:23 +0100

    changed files

--- custom_tools/results_checker.py
+++ custom_tools/results_checker.py
@@ -1,8 +1,8 @@
 import mmcv
 import numpy as np
 
-img = mmcv.imread('data/boulderColabFormat/labels/03_02_00750.png')
-res = mmcv.imread('data/results/03_02_00750.png')
+img = mmcv.imread('data/boulderColabFormat/labels/02_01_02650.png')
+res = mmcv.imread('data/results/02_01_02650.png')
 res_live = mmcv.imread('data/live_test.png')
 
 # res = np.floor(res * res * 63.75).astype('uint8')
@@ -10,5 +10,4 @@ img = np.floor(img * 127.5).astype('uint8')
 res = np.floor(res * 127.5).astype('uint8')
 res_live = np.floor(res_live * 127.5).astype('uint8')
 
-mmcv.imwrite(res, 'data/ground_truth_03_02_00750.png')
-pip
+mmcv.imwrite(res, 'data/02_01_02650.png')
