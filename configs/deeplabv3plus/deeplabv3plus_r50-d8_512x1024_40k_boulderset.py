4cd9be0c8113c29d3026b3eb8061f784ffd7eeaa
Jan <j.schiff96@gmail.com>
2021-04-20 11:32:46 +0200

    refactor

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
@@ -1,7 +1,8 @@
 _base_ = [
     # '../_base_/models/deeplabv3plus_r18-d8.py',
     '../_base_/models/deeplabv3plus_r50-d8.py',
-    '../_base_/datasets/boulderset.py', '../_base_/default_runtime.py',
+    '../_base_/datasets/boulderset.py',
+    '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_40k.py'
 ]
 
