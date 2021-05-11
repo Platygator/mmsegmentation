ae9499619c1b94938a2cf484d72a1a2fe1965e24
Jan <j.schiff96@gmail.com>
2021-05-11 15:47:46 +0200

    added unknown with index 50 and added it to the ignore index

--- mmseg/datasets/boulderset.py
+++ mmseg/datasets/boulderset.py
@@ -5,7 +5,7 @@ import os.path as osp
 
 @DATASETS.register_module()
 class BoulderDataset(CustomDataset):
-    CLASSES = ("background", "stone", "border")
+    CLASSES = ("background", "stone", "border", "unknown")
     # PALETTE = [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
     PALETTE = [[0, 0, 0], [128, 128, 128], [255, 255, 255], [50, 50, 50]]
 
