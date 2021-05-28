94e8e9d0ff93bc5d24ed73f8f0eab53c1462ee91
Jan <j.schiff96@gmail.com>
2021-05-28 16:57:51 +0200

    rollback to iter based

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
@@ -2,8 +2,8 @@ _base_ = [
     # '../_base_/models/deeplabv3plus_r18-d8.py',
     '../_base_/models/deeplabv3plus_r50-d8.py',
     '../_base_/datasets/boulderset.py',
-    # '../_base_/default_runtime.py',
-    # '../_base_/schedules/schedule_40k.py'
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_40k.py'
 ]
 
 norm_cfg = dict(type='BN', requires_grad=True)
@@ -31,32 +31,18 @@ model = dict(
         num_classes=num_classes,
         norm_cfg=norm_cfg
     ))
-
-optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict()
-# learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=True)
-
 runner = dict(
-    type='EpochBasedRunner', # Type of runner to use (i.e. IterBasedRunner or EpochBasedRunner)
-    max_epochs=100)  # Total number of iterations. For EpochBasedRunner use `max_epochs`
-checkpoint_config = dict(   # Config to set the checkpoint hook, Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
-    by_epoch=True,  # Whethe count by epoch or not.
-    interval=10)  # The save interval.
-evaluation = dict(   # The config to build the evaluation hook. Please refer to mmseg/core/evaulation/eval_hook.py for details.
-    interval=2,
-    # by_epoch=True,  # The interval of evaluation.
+    type='IterBasedRunner', # Type of runner to use (i.e. IterBasedRunner or EpochBasedRunner)
+    max_iters=50000) # Total number of iterations. For EpochBasedRunner use `max_epochs`
+checkpoint_config = dict(  # Config to set the checkpoint hook, Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
+    by_epoch=False,  # Whethe count by epoch or not.
+    interval=4000)  # The save interval.
+evaluation = dict(  # The config to build the evaluation hook. Please refer to mmseg/core/evaulation/eval_hook.py for details.
+    interval=4000,  # The interval of evaluation.
     metric='mIoU')
 log_config = dict(  # config to register logger hook
-    interval=1,  # Interval to print the log
+    interval=50,  # Interval to print the log
     hooks=[
         # dict(type='TensorboardLoggerHook')  # The Tensorboard logger is also supported
-        dict(type='TextLoggerHook', by_epoch=True)
+        dict(type='TextLoggerHook', by_epoch=False)
     ])
-# yapf:enable
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-load_from = None
-resume_from = None
-workflow = [('train', 1)]
-cudnn_benchmark = True
