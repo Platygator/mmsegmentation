32cca2691cdaff42f0c4cc871386d80ce2d21383
Jan <j.schiff96@gmail.com>
2021-06-08 12:42:13 +0200

    refactoring

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x1024_40k_boulderset.py
@@ -35,16 +35,20 @@ model = dict(
 # optimizer
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
+
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=True)
 
+# Runner
 runner = dict(
     type='EpochBasedRunner', # Type of runner to use (i.e. IterBasedRunner or EpochBasedRunner)
-    max_epochs=100) # Total number of iterations. For EpochBasedRunner use `max_epochs`
-checkpoint_config = dict(  # Config to set the checkpoint hook, Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
+    max_epochs=100)  # Total number of iterations. For EpochBasedRunner use `max_epochs`
+checkpoint_config = dict(  # Config to set the checkpoint hook,
+    # Refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/hooks/checkpoint.py for implementation.
     by_epoch=True,  # Whether count by epoch or not.
     interval=4)  # The save interval.
-evaluation = dict(  # The config to build the evaluation hook. Please refer to mmseg/core/evaulation/eval_hook.py for details.
+evaluation = dict(  # The config to build the evaluation hook.
+    # Please refer to mmseg/core/evaulation/eval_hook.py for details.
     interval=4,  # The interval of evaluation.
     metric='mIoU')
 log_config = dict(  # config to register logger hook
