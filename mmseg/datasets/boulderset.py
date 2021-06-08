4034329d39c91dd34234f4dd924abeb00ed9ba58
Jan <j.schiff96@gmail.com>
2021-06-08 12:57:49 +0200

    changed palette to 1,2,3

--- mmseg/datasets/boulderset.py
+++ mmseg/datasets/boulderset.py
@@ -8,8 +8,8 @@ class BoulderDataset(CustomDataset):
     # TODO should my unknown label be named here?
     #      why does using the visual labels (commented) not work?
     CLASSES = ("background", "stone", "border", "unknown")
-    PALETTE = [[0, 0, 0], [128, 128, 128], [255, 255, 255], [50, 50, 50]]
-    # PALETTE = [[0, 0, 0], [1, 1, 1], [2, 2, 2], [3, 3, 3]]
+    # PALETTE = [[0, 0, 0], [128, 128, 128], [255, 255, 255], [50, 50, 50]]
+    PALETTE = [[0, 0, 0], [1, 1, 1], [2, 2, 2], [3, 3, 3]]
 
     def __init__(self, split, **kwargs):
         super().__init__(ignore_index=3, img_suffix='.png', seg_map_suffix='.png',
